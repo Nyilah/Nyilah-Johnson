50ed795082b2ffd2101182b734ac01c39faab4f7
Nyilah Johnson <Nyilahtjohnson@gmail.com>
2019-09-24 20:17:51 -0500

    add a root div and try the promp function

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// function greeter(question = "What is your name?") {
+//   let answer = prompt(question);
+
+//   if (answer === "") {
+//     greeter(); // recursion
+//   }
+
+//   return answer;
+// }
+
+let greet = prompt(question = "What is your name?");
+
+alert("hey" + greet);
+// function greeter(question) {
+//     let answer = greet;
+
+//     if(answer === "") {
+//     greeter();
+//     }
+//     return answer;
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const answer = greeter();
+
+// function greeter(question = "what is your name") {
+//     if (answer === "") {
+//       console.log('you did not answer!');
+//     }
+//     document.querySelector('h1').textContent = greeter()
+//   return prompt(question);
+
+  // const answer = greeter();
+  // document.querySelector('h1').textContent = greeter();
+// console.log(answer);
+
+
+
+// let i = 1;
+
+// let total =0;
+
+// while(<= 10)
+//   total += i;
+//   i++;
+//
