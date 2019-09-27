4d850eb1f4df3a3855fc8a73360cf3f602a3517f
Nyilah Johnson <Nyilahtjohnson@gmail.com>
2019-09-26 20:28:23 -0500

    add components directory and add files to it for single page application

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+export default `<header>
+<figure>
+  <img src="brendan-digital-strategist-GfFFOqiTH4M-unsplash.jpg" alt="" />
+  <!-- srcset="
+      brendan-digital-strategist-GfFFOqiTH4M-unsplash.jpg 200,
+      brendan-digital-strategist-GfFFOqiTH4M-unsplash.jpg/300 1.5x,
+      brendan-digital-strategist-GfFFOqiTH4M-unsplash.jpg/400 2x,
+      brendan-digital-strategist-GfFFOqiTH4M-unsplash.jpg/600 3x
+      " -->
+</figure>
+<h1>Nyilah Johnson's Project</h1>
+</header>`
