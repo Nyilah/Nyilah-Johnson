ab1be5338fcb285d333d13741fb20937c93bbc23
Nyilah Johnson <Nyilahtjohnson@gmail.com>
2019-09-26 20:48:05 -0500

    download parcel

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export default `<footer>
+      <ul class="footer">
+        <li><a href="./">Home</a></li>
+        <li><a href="./About">About</a></li>
+        <li><a href="./Contact">Contact</a></li>
+        <li><a href="./Gallery">Gallery</a></li>
+        <li><a href="./Blog">Blog</a></li>
+      </ul>
+      <p>&copy; Nyilah Johnson no rights reserved</p>
+    </footer>`
---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+export default `<nav>
+      <span class="fas fa-hamburger is-hidden--desktop"></span>
+      <ul class="is-hidden--mobile is-hidden--tablet is-shown--desktop">
+        <li><a href="./">Home</a></li>
+        <li><a href="./About">About</a></li>
+        <li><a href="./Contact">contact</a></li>
+        <li><a href="./Gallery">Gallery</a></li>
+        <li><a href="./Blog">Blog</a></li>
+      </ul>
+    </nav>`
